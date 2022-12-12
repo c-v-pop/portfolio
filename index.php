48d9e2b6b6893d7a9676585d097bf12f390b0346
noanswerss <ciprianvlad888@gmail.com>
2022-12-12 17:21:14 +0000

    added projects description

--- index.php
+++ index.php
@@ -1,32 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="./dist/output.css">
-    <title>Ciprian Pop's Portofolio</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="./dist/output.css">
+<title>Ciprian Pop's Portofolio</title>
 </head>
-<body class="bg-blue-400">
-    
-    <?php
-        $introduction = "Hello world";
-        $books = [
-            "Random book",
-            "Nice book",
-            "Awful book"
-        ]
-    ?>
-    <div>
-    <h1 class="bg-green-200">Hello dude</h1>
-        <?= $introduction; ?>
-        <ul>
-            <?php 
-            foreach ($books as $book)
-            {
-                // echo $book;
-                    echo "<li>$book</li>";
-            }
-            ?>
-        </ul>
-    </div>
+<body>
+    <header>
+        <nav class="flex flex-wrap justify-evenly text-lg my-5">
+            <a href="projects.php">Projects</a>
+            <a href="">C.V.</a>
+            <a href="">Experience</a>
+            <a href="">Contact</a>
+        </nav>
+        <div class="mx-auto w-1/6">
+            <img src="images/logo.PNG" alt="logo">   
+            <h1 class="text-center">Ciprian Pop</h1>
+            <p class="text-center">Web Developer</p>
+        </div>
+    </header>   
+    <main>
+        <section>
+            <div> 
+                <h2>Turn Mock-ups Into a Web Page with HTML & CSS</h2>
+                <p>Reservia: Rental Marketplace
+
+Transformed mockup into responsive one-page HTML/CSS static template
+
+Live page @ https://noanswerss.github.io/Reservia/
+
+Coded in Visual Studio Code and used:
+
+HTML5 Semantic Tags CSS3 Flexbox CSS3 Grid Layout CSS3 Custom Media Queries W3C Html & Css Validator</p>
+            </div>
+            <div> 
+                <h2>Integrate a mobile website with animations in CSS</h2>
+                <p>Ohmyfood is a new startup that wants to make a name for itself in the restaurant business. The objective is to develop a 100% mobile-friendly site that lists the menus of gourmet restaurants. In addition to having a classic reservation system, 
+                    customers will be able to select the dishes they want for their meal so that they are ready when they arrive. No more wait times in restaurants!</p>
+            </div>
+            <div> 
+                <h2>Optimize an Existing Website</h2>
+            </div>
+            <div> 
+                <h2>Build an e-commerce Website with JavaScript</h2>
+                <p>You recently started working at a web development agency. During the first few weeks, you successfully developed a few websites using HTML/CSS and now you have been assigned a new task.
+
+Your client is Kanap, a furniture company which sells its products from their physical shop only. The company would now like to add an online store to its physical shop in order to sell its products on the Internet.</p>
+            </div>
+            <div> 
+                <h2>Build a Secure API for a Review App</h2>
+                <p>You have spent the past year or so as a freelance back-end developer, and you have worked on multiple projects of varying sizes and difficulties.</p>
+            </div>
+        </section>
+    </main>    
 </body>
 </html>
