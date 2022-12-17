9de77343f54774b52c143f80c16f47d0ad3f63b8
noanswerss <ciprianvlad888@gmail.com>
2022-12-17 14:21:03 +0000

    changed background

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="./dist/output.css">
 <title>Ciprian Pop's Portofolio</title>
 </head>
-<body>
+<body class="bg-gradient-to-t from-blue-400 border-t-yellow-50">
     <header>
         <nav class="flex flex-wrap justify-evenly text-lg my-5">
             <a href="projects.php">Projects</a>
@@ -15,7 +15,7 @@
             <a href="mailto:ciprianvlad888@gmail.com?subject=The%20subject%20of%20the%20mail">Contact</a>
         </nav>
         <div class="mx-auto w-1/6">
-            <img class="hover:animate-bounce" src="images/logo.PNG" alt="logo">    
+            <img class="hover:animate-ping duration-1000 delay-1000" src="images/logo.PNG" alt="logo">    
             <h1 class="text-center">Ciprian Pop</h1>
             <p class="text-center">Web Developer</p>
         </div>
