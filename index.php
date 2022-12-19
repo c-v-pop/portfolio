40a13caac6dcb03c376c951016b73365a65f7cde
noanswerss <ciprianvlad888@gmail.com>
2022-12-19 17:01:02 +0000

    done some animations

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
             <a href="projects.php" class="py-5 px-10 text-2xl">Projects</a>
             <a href="http://example.com/files/myfile.pdf  target="_blank" class="py-5 px-10 text-2xl">C.V.</a>
             <a href="" class="py-5 px-10 text-2xl">Experience</a>
-            <a href="mailto:ciprianvlad888@gmail.com?subject=The%20subject%20of%20the%20mail" class="py-5 px-10 text-2xl">Contact</a>
+            <a href="mailto:ciprianvlad888@gmail.com?subject=The%20subject%20of%20the%20mail" class="py-5 px-10 text-2xl">E-mail</a>
         </nav>
-        <div class="mx-1 w-1/6 mt-2 fixed bottom-0 right-0">
+        <div class="mx-1 h-1/5 w-1/5 mt-2 fixed top-0 left-0">
             <img class="rounded-full hover:p-0 transition-none duration-500" src="images/think.PNG" alt="logo">     
             <h1 class="text-white uppercase text-center">Ciprian Pop</h1>
         </div>
@@ -22,45 +22,47 @@
     <main>
         <section class="flex mx-40 my-2 text-center flex-wrap">
             <div class="flex justify-center flex-col mx-auto">
-                <p class="flex justify-center text-red-400  px-40 mx-40 text-sm text-center">Experienced web developer adept in all stages of advanced web development. </p>
-                    <p class="flex justify-center text-red-400  px-40 mx-40 text-sm text-center">Knowledgeable in user interface, testing, and debugging processes.</p>
-                      <p class="flex justify-center text-red-400  px-40 mx-40 text-sm text-center"> Bringing forth expertise in design, installation, testing
+                <img src="" alt="">
+                <p class="flex justify-center text-white  px-40 mx-40 text-lg text-center ">Experienced web developer adept in all stages of advanced web development. </p>
+                    <p class="flex justify-center text-white  px-40 mx-40 text-lg text-center">Knowledgeable in user interface, testing, and debugging processes.</p>
+                      <p class="flex justify-center text-white  px-40 mx-40 text-lg text-center"> Bringing forth expertise in design, installation, testing
                         and maintenance of web systems.</p>
             </div>
         </section>
         <h2 class="text-center uppercase text-2xl font-serif font-thin">Frontend development</h2>
-        <ul class="flex flex-row mg py-4 gap-6">
+        <ul class="flex flex-row mg py-4 gap-6 justify-evenly">
             <a href="https://noanswerss.github.io/Reservia/" class="space-x-3.5 bg-slate-600">
-        <li class="text-white text-center">Reservia</li>
+        <li class="text-white text-center break-words">Reservia</li>
         <p class="text-white">Hotels Accommodations and Activities</p>
         <p> Transformed mockup into responsive one-page HTML/CSS static template</p>
             </a>
-            <a href="http://" class="space-x-3.5">
-        <li class="text-red-200 text-center">OhMyFood</li>
-        <p class="text-red-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
-            Laborum odio inventore quasi iure distinctio placeat iste recusandae maiores, architecto quae, adipisci eum dolorem repellendus saepe ratione quia.</p>
+            <a href="https://noanswerss.github.io/ohmyfoodscss/" class="space-x-3.5 bg-slate-600">
+        <li class="text-white text-center">OhMyFood</li>
+        <p>Integrate a mobile website with animations in CSS</p>
+        <p> web page hosted online on GitHub Pages </p>
+        <p>Custom made Graphic Effects and Animations</p>
             </a>
-            <a href="http://">
-        <li class="text-blue-400 text-center">GoMike</li>
-        <p class="text-blue-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
+            <a href="https://noanswerss.github.io/GoMikeDesigns/" class="space-x-3.5 bg-slate-600">
+        <li class="text-center">GoMike</li>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
             Laborum odio inventore quasi iure distinctio placeat iste recusandae maiores, architecto quae, adipisci eum dolorem repellendus saepe ratione quia.</p>
             </a>
         </ul>
         <h2 class="text-center text-2xl uppercase font-serif font-extrabold">Backend development</h2>
-        <ul class="flex flex-row mg py-4">
-            <a href="http://">
-        <li class="text-yellow-200 text-center">Kanap</li>
-        <p class="text-yellow-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
+        <ul class="flex flex-row mg py-4 gap-6">
+            <a href="http://" class="space-x-3.5 bg-slate-400">
+        <li class="text-center">Kanap</li>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
             Laborum odio inventore quasi iure distinctio placeat iste recusandae maiores, architecto quae, adipisci eum dolorem repellendus saepe ratione quia.</p>
             </a>
-            <a href="http://">
-        <li class="text-orange-500 text-center">Piquante</li>
-        <p class="text-orange-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
+            <a href="http://"  class="space-x-3.5 bg-slate-400">
+        <li class="text-center">Piquante</li>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
             Laborum odio inventore quasi iure distinctio placeat iste recusandae maiores, architecto quae, adipisci eum dolorem repellendus saepe ratione quia.</p>
             </a>
-            <a href="http://">
-        <li class="text-purple-600 text-center">Groupomania</li>
-        <p class="text-purple-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
+            <a href="http://"  class="space-x-3.5 bg-slate-400">
+        <li class="text-center">Groupomania</li>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
             Laborum odio inventore quasi iure distinctio placeat iste recusandae maiores, architecto quae, adipisci eum dolorem repellendus saepe ratione quia.</p>
             </a>
     </ul>
