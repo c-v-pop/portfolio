db47c99f3575f6e246e68cf703cabba3e0d199c6
noanswerss <ciprianvlad888@gmail.com>
2022-12-19 16:21:29 +0000

    added animations and project description and images

--- index.php
+++ index.php
@@ -6,25 +6,63 @@
 <link rel="stylesheet" href="./dist/output.css">
 <title>Ciprian Pop's Portofolio</title>
 </head>
-<body class="bg-gradient-to-t from-blue-400 border-t-yellow-50">
+<body class="bg-slate-500">
     <header>
-        <nav class="flex flex-wrap justify-evenly text-lg my-5">
-            <a href="projects.php">Projects</a>
-            <a href="">C.V.</a>
-            <a href="">Experience</a>
-            <a href="mailto:ciprianvlad888@gmail.com?subject=The%20subject%20of%20the%20mail">Contact</a>
+        <nav class="flex flex-wrap justify-evenly text-lg py-2">
+            <a href="projects.php" class="py-5 px-10 text-2xl">Projects</a>
+            <a href="http://example.com/files/myfile.pdf  target="_blank" class="py-5 px-10 text-2xl">C.V.</a>
+            <a href="" class="py-5 px-10 text-2xl">Experience</a>
+            <a href="mailto:ciprianvlad888@gmail.com?subject=The%20subject%20of%20the%20mail" class="py-5 px-10 text-2xl">Contact</a>
         </nav>
-        <div class="mx-auto w-1/6">
-            <img class="hover:animate-ping duration-1000 delay-1000" src="images/logo.PNG" alt="logo">    
-            <h1 class="text-center">Ciprian Pop</h1>
-            <p class="text-center">Web Developer</p>
+        <div class="mx-1 w-1/6 mt-2 fixed bottom-0 right-0">
+            <img class="rounded-full hover:p-0 transition-none duration-500" src="images/think.PNG" alt="logo">     
+            <h1 class="text-white uppercase text-center">Ciprian Pop</h1>
         </div>
     </header>   
     <main>
-        <section class="flex justify-center">
-            <div>
-                <h1>This will have my description</h1>
+        <section class="flex mx-40 my-2 text-center flex-wrap">
+            <div class="flex justify-center flex-col mx-auto">
+                <p class="flex justify-center text-red-400  px-40 mx-40 text-sm text-center">Experienced web developer adept in all stages of advanced web development. </p>
+                    <p class="flex justify-center text-red-400  px-40 mx-40 text-sm text-center">Knowledgeable in user interface, testing, and debugging processes.</p>
+                      <p class="flex justify-center text-red-400  px-40 mx-40 text-sm text-center"> Bringing forth expertise in design, installation, testing
+                        and maintenance of web systems.</p>
             </div>
         </section>
+        <h2 class="text-center uppercase text-2xl font-serif font-thin">Frontend development</h2>
+        <ul class="flex flex-row mg py-4 gap-6">
+            <a href="https://noanswerss.github.io/Reservia/" class="space-x-3.5 bg-slate-600">
+        <li class="text-white text-center">Reservia</li>
+        <p class="text-white">Hotels Accommodations and Activities</p>
+        <p> Transformed mockup into responsive one-page HTML/CSS static template</p>
+            </a>
+            <a href="http://" class="space-x-3.5">
+        <li class="text-red-200 text-center">OhMyFood</li>
+        <p class="text-red-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
+            Laborum odio inventore quasi iure distinctio placeat iste recusandae maiores, architecto quae, adipisci eum dolorem repellendus saepe ratione quia.</p>
+            </a>
+            <a href="http://">
+        <li class="text-blue-400 text-center">GoMike</li>
+        <p class="text-blue-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
+            Laborum odio inventore quasi iure distinctio placeat iste recusandae maiores, architecto quae, adipisci eum dolorem repellendus saepe ratione quia.</p>
+            </a>
+        </ul>
+        <h2 class="text-center text-2xl uppercase font-serif font-extrabold">Backend development</h2>
+        <ul class="flex flex-row mg py-4">
+            <a href="http://">
+        <li class="text-yellow-200 text-center">Kanap</li>
+        <p class="text-yellow-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
+            Laborum odio inventore quasi iure distinctio placeat iste recusandae maiores, architecto quae, adipisci eum dolorem repellendus saepe ratione quia.</p>
+            </a>
+            <a href="http://">
+        <li class="text-orange-500 text-center">Piquante</li>
+        <p class="text-orange-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
+            Laborum odio inventore quasi iure distinctio placeat iste recusandae maiores, architecto quae, adipisci eum dolorem repellendus saepe ratione quia.</p>
+            </a>
+            <a href="http://">
+        <li class="text-purple-600 text-center">Groupomania</li>
+        <p class="text-purple-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima explicabo iste eum, assumenda et corporis, consequatur, repellat modi debitis architecto illo quaerat sit odio dolorem voluptatibus hic provident fuga aliquid alias. 
+            Laborum odio inventore quasi iure distinctio placeat iste recusandae maiores, architecto quae, adipisci eum dolorem repellendus saepe ratione quia.</p>
+            </a>
+    </ul>
 </body>
 </html>
--- projects.php
+++ projects.php
@@ -4,11 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
     <title>Projects</title>
 </head>
 <body>
-    <div>
+    <section>
+    <div class="flex">
         <h1>These are my Projects</h1>
     </div>
+    <ul>
+        <li>Reservia</li>
+        <li>OhMyFood</li>
+        <li>GoMike</li>
+        <li>Kanap</li>
+        <li>Piquante</li>
+        <li>Groupomania</li>
+    </ul>
+    </section>
 </body>
 </html>
