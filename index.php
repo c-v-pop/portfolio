0a0268a0bb601ff72d23089f7c2064dd49d8b7ef
noanswerss <ciprianvlad888@gmail.com>
2022-12-20 09:26:42 +0000

    made cards for projects

--- index.php
+++ index.php
@@ -8,20 +8,19 @@
 </head>
 <body class="bg-slate-500">
     <header>
-        <nav class="flex flex-wrap justify-evenly text-lg py-2">
+        <nav class="flex flex-wrap justify-evenly text-lg py-2 my-3">
             <a href="projects.php" class="py-5 px-10 text-2xl">Projects</a>
             <a href="http://example.com/files/myfile.pdf  target="_blank" class="py-5 px-10 text-2xl">C.V.</a>
             <a href="" class="py-5 px-10 text-2xl">Experience</a>
             <a href="mailto:ciprianvlad888@gmail.com?subject=The%20subject%20of%20the%20mail" class="py-5 px-10 text-2xl">E-mail</a>
         </nav>
-        <div class="flex justify-center flex-col mx-auto">
-            <img src="" alt="">
+            <div class="flex justify-center flex-col mx-auto bg-slate-800 w-2/3 rounded-md">
             <p class="flex justify-center text-white  px-40 mx-40 text-lg text-center ">Experienced web developer adept in all stages of advanced web development. </p>
             <p class="flex justify-center text-white  px-40 mx-40 text-lg text-center">Knowledgeable in user interface, testing, and debugging processes.</p>
             <p class="flex justify-center text-white  px-40 mx-40 text-lg text-center"> Bringing forth expertise in design, installation, testing
                 and maintenance of web systems.</p>
             </div>
-            <img class="rounded-full hover:p-0 transition-none duration-500 w-20 mx-auto" src="images/think.PNG" alt="logo">     
+            <img class="rounded-full hover:p-0 transition-none duration-500 w-44 mx-auto" src="images/think.PNG" alt="logo">     
             <div class="justify-center flex">
                 <h1 class="text-white uppercase text-center">Ciprian Pop</h1>
             </div>
@@ -32,17 +31,26 @@
         <h2 class="text-center uppercase text-2xl font-serif font-thin">Frontend development</h2>
         <ul class="flex flex-row mg py-4 gap-6 justify-evenly">
             <a href="https://noanswerss.github.io/Reservia/" class="space-x-3.5 bg-slate-600">
-        <li class="text-white text-center break-words">Reservia</li>
+                <li class="text-white text-center break-words">
+                   <h2> Reservia </h2>
+                </li>
+                <img src="images/htmlcss.webp" alt="Html and Css logo" class="sm p-5 rounded-xl">
         <p class="text-white">Hotels Accommodations and Activities</p>
         <p> Transformed mockup into responsive one-page HTML/CSS static template</p>
             </a>
             <a href="https://noanswerss.github.io/ohmyfoodscss/" class="space-x-3.5 bg-slate-600">
-        <li class="text-white text-center">OhMyFood</li>
-        <p>Integrated a mobile website with animations in CSS</p>
-        <p>Custom made Graphic Effects and Animations</p>
+                <li class="text-white text-center">
+                    <h2>OhMyFood</h2>
+                </li>
+                <img src="images/sass.png" alt="Sass logo" class="sm p-5">
+                <p>Integrated a mobile website with animations in CSS</p>
+            <p>Custom made Graphic Effects and Animations</p>
             </a>
             <a href="https://noanswerss.github.io/GoMikeDesigns/" class="space-x-3.5 bg-slate-600">
-        <li class="text-center">GoMike</li>
+                <li class="text-center">
+            <h2>GoMikeDesigns</h2>
+        </li>
+                <img src="images/seo.png" alt="" class="sm p-5">
         <p>Optimized an existing website using S.E.O. techniques</p>
         <p></p>
             </a>
@@ -50,21 +58,20 @@
         <h2 class="text-center text-2xl uppercase font-serif font-extrabold">Backend development</h2>
         <ul class="flex flex-row mg py-4 gap-6">
             <a href="http://" class="space-x-3.5 bg-slate-400">
+            <img src="images/js.png" alt="" class="sm p-5">
         <li class="text-center">Kanap</li>
         <p>First attempt at a fullstack application where I got to work with an API and fetch products from the backend to display dynamically</p>
             </a>
             <a href="http://"  class="space-x-3.5 bg-slate-400">
+            <img src="images/node.png" alt="" class="sm p-5">
         <li class="text-center">Piquante</li>
         <p>Build the back-end and API for 'Piquante', a new application from So Pekocko that allows users to like/dislike sauces.</p>
             </a>
             <a href="http://"  class="space-x-3.5 bg-slate-400">
+            <img src="images/react.png" alt="" class="sm  p-5">
         <li class="text-center">Groupomania</li>
-        <p>The project will be to build an internal social network for Groupomania's employees. The goal of this tool is to facilitate more interaction between colleagues. Groupomania's HR department has envisioned two applications for promoting exchanges between colleagues. Otherwise, they’re allowing for a lot of flexibility regarding the functionalities of the network.
-
-Stephanie describes the proposed apps, and you realize that these are spin-offs of products that already exist:
-
-9GAG, because they want employees to share and comment on gifs with other colleagues.
-Reddit, because they want employees to write and/or share articles with colleagues on topics of interest to them.</p>
+        <p>The project will be to build an internal social network for Groupomania's employees. </p>
+        <p> The goal of this tool is to facilitate more interaction between colleagues. </p>
             </a>
     </ul>
 </body>
