0ae403b36c16eb618d6cbcd4729929e54667a08d
noanswerss <ciprianvlad888@gmail.com>
2023-01-29 17:14:35 +0000

    added responsiverness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["index.php", "styles/index.css"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-bg': '#243C5A',
+        'primary': '#64FFDA',
+        'support-200': '#8892B0',
+        'support': '#CCD6F6',
+      },
+    },
   },
   plugins: [],
 }
